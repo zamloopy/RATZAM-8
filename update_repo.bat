2f7f42f9ca18524d94d2d3a0d79e393a9c436690
zamloopy <13zamloopy@gmail.com>
2025-09-16 20:12:28 -0400

    Auto-commit Tue 09/16/2025 20:12:28.45

--- update_repo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd "C:\Users\YourUser\Documents\RATZAM-8"
-git pull origin main
-git add .
-git commit -m "Auto-commit %date% %time%" 
-git push origin main
-echo RATZAM-8 project pushed successfully!
-pause
-
